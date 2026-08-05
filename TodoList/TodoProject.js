56bc5ba26d9d86309c5dde32fb7ee4d3489d6187
Nadim Bhuiyan <bhuiyan.nadim9@gmail.com>
2026-08-05 21:11:11 +0600

    add Todo List Project

---
+++ TodoList/TodoProject.js
@@ -0,0 +1,31 @@
+        let enteredValue = [];
+        
+        function takingValue(){
+            let name = document.querySelector('#input').value;
+            let due = document.querySelector('#due').value;
+            enteredValue.push({name: name,
+                due: due
+            });
+            document.querySelector('#input').value = '';
+            document.querySelector('#due').value = '';
+        }
+
+        function todoFunc(){
+        let showText = '';
+        for(let i =0; i<enteredValue.length; i++)
+        {
+            let todo = enteredValue[i];
+            let html = `<div class="name-date-add">
+                            <div>${todo.name} </div>
+                            <div>${todo.due} </div>
+                            <button id="delete" onclick="
+                                enteredValue.splice(${i}, 1);
+                                todoFunc();
+                            ">Delete
+                            </button>
+                        </div>`;
+            showText += html;
+        }
+        let value = document.querySelector('#show');
+        value.innerHTML = showText;
+    }
